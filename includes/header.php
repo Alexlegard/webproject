b340de7d298b7eb656ff8f25bbb4301cafecf6b8
admbjr <36055206+admbjr@users.noreply.github.com>
2019-06-10 22:55:48 -0400

    logout in nav

--- includes/header.php
+++ includes/header.php
@@ -23,8 +23,8 @@
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="nav navbar-nav ml-auto text-uppercase">
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="index.php">search</a></li>
-                    <!-- <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="contact.php">Contact</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="login.php">Logout</a></li> -->
+                    <!-- <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="contact.php">Contact</a></li> -->
+                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="login.php">Logout</a></li>
                 </ul>
             </div>
         </div>
